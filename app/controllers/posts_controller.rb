60a066a09512cf632574864520db07b192c26c16
Gabriel Quesada <gab-56@hotmail.com>
2019-05-10 09:52:15 -0600

    Small fix in destroy post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@
 
 class PostsController < ApplicationController
 
-  before_action :set_post, only: %i[show edit update]
+  before_action :set_post, only: %i[show edit update destroy]
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
